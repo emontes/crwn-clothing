1c5603371ecd370a4d78f1df39773e36d23bd9d7
emontes <emontes@turista.com.mx>
2020-05-22 13:13:37 -0500

    separating our cra app into client folder, added express server to help handle stripe payment

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_f7AxjJhR8gEctPd5MJfzqZ8400RY555UbH';
 
     const onToken = token => {
-        console.log(token)
-        alert('Payment Successful');
+        axios({
+            url: 'payment',
+            method: 'post',
+            data: {
+                amount: priceForStripe,
+                token
+            }
+        }).then(response => {
+            alert('Payment successful')
+        }).catch(error => {
+            console.log('Payment error: ', JSON.parse(error));
+            alert(
+                'There was an issue with your payment. Please sure you use the provided credit card'
+            );
+        }) 
     }
 
     return (
