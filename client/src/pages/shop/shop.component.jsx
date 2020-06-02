382e8301ad6d14bae97d7b2869312dedccb8671c
emontes <emontes@turista.com.mx>
2020-06-02 16:19:37 -0500

    adding performance optimizations to codebase with react lazy, suspense and memo

--- client/src/pages/shop/shop.component.jsx
+++ client/src/pages/shop/shop.component.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, lazy, Suspense } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
+import Spinner from '../../components/spinner/spinner.component';
 
-import CollectionsOverviewContainer from '../../components/collections-overview/collections-overview.container';
-import CollectionPageContainer from '../collection/collection.container';
+const CollectionsOverviewContainer = lazy(() => import('../../components/collections-overview/collections-overview.container'));
+const CollectionPageContainer = lazy(() => import('../collection/collection.container'));
+
 
 const ShopPage = ( {match, fetchCollectionsStart} ) => {
   useEffect(() => {
@@ -14,6 +16,7 @@ const ShopPage = ( {match, fetchCollectionsStart} ) => {
 
     return (
       <div className='shop-page'>
+        <Suspense fallback={<Spinner />}>
         <Route
           exact
           path={`${match.path}`}
@@ -23,6 +26,7 @@ const ShopPage = ( {match, fetchCollectionsStart} ) => {
           path={`${match.path}/:collectionId`}
           component={CollectionPageContainer}
         />
+        </Suspense>
       </div>
     );
   
